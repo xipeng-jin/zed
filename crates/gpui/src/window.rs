a9322dcb812fd336d879ed1dc40053638fa98b9a
xipengjin <jinxp18@gmail.com>
2025-11-11 21:09:57 -0500

    Toggle macOS traffic light with title bar

--- crates/gpui/src/window.rs
+++ crates/gpui/src/window.rs
@@ -4782,6 +4782,12 @@ impl Window {
             .set_tabbing_identifier(tabbing_identifier)
     }
 
+    /// Toggle visibility of the native macOS traffic-light controls
+    /// This is macOS specific
+    pub fn set_traffic_light_visible(&self, visible: bool) {
+        self.platform_window.set_traffic_light_visible(visible);
+    }
+
     /// Toggles the inspector mode on this window.
     #[cfg(any(feature = "inspector", debug_assertions))]
     pub fn toggle_inspector(&mut self, cx: &mut App) {
