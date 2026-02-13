16dfc60ad2c9e5f4f9388acaf8cecb3d45aeafe5
Jakub Konka <kubkon@jakubkonka.com>
2026-02-13 20:16:11 +0100

    util: Always use posix_spawn on macOS even with pre_exec hooks (#49090)
    Here's some backstory:
    * on macOS, @cole-miller and I noticed that since roughly Oct 2025, due
    to some changes to latest macOS Tahoe, for any spawned child process we
    needed to reset Mach exception ports
    (https://github.com/zed-industries/zed/issues/36754 +
    https://github.com/RemiKalbe/zed/commit/6e8f2d2ebe5a93753625a3026aeb996de8cb436b)
    * the changes in that PR achieve that via `pre_exec` hook on
    `std::process::Command` which then abandons `posix_spawn` syscall for
    `fork` + `execve` dance on macOS (we tracked it down in Rust's std
    implementation)
    * as it turns out, `fork` + `execve` is pretty expensive on macOS
    (apparently way more so than on other OSes like Linux) and `fork` takes
    a process-wide lock on the allocator which is bad
    * however, since we wanna reset exception ports on the child, the only
    official way supported by Rust's std is to use `pre_exec` hook
    * posix_spawn on macOS exposes this tho via a macOS specific extension
    to that syscall `posix_spawnattr_setexceptionports_np` but there is no
    way to use that via any standard interfaces in `std::process::Command`
    * thus, it seemed like a good idea to instead create our own custom
    Command wrapper that on non-macOS hosts is a zero-cost wrapper of
    `smol::process::Command`, while on macOS we reimplement the minimum to
    achieve `smol::process::Command`  with `posix_spawn` under-the-hood
    
    Notably, this changeset improves git-blame in very large repos
    significantly.
    
    Release Notes:
    
    - Fixed performance spawning child processes on macOS by always forcing
    `posix_spawn` no matter what.
    
    ---------
    
    Co-authored-by: Cole Miller <cole@zed.dev>

--- crates/project/src/lsp_store.rs
+++ crates/project/src/lsp_store.rs
@@ -2373,7 +2373,8 @@ impl LocalLspStore {
             Some(worktree_path)
         });
 
-        let mut child = util::command::new_smol_command(command);
+        use util::command::Stdio;
+        let mut child = util::command::new_command(command);
 
         if let Some(buffer_env) = buffer.env.as_ref() {
             child.envs(buffer_env);
@@ -2394,9 +2395,9 @@ impl LocalLspStore {
         }
 
         let mut child = child
-            .stdin(smol::process::Stdio::piped())
-            .stdout(smol::process::Stdio::piped())
-            .stderr(smol::process::Stdio::piped())
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
             .spawn()?;
 
         let stdin = child.stdin.as_mut().context("failed to acquire stdin")?;
@@ -14100,7 +14101,7 @@ impl LspAdapterDelegate for LocalLspAdapterDelegate {
         };
 
         let env = self.shell_env().await;
-        let output = util::command::new_smol_command(&npm)
+        let output = util::command::new_command(&npm)
             .args(["root", "-g"])
             .envs(env)
             .current_dir(local_package_directory)
@@ -14135,7 +14136,7 @@ impl LspAdapterDelegate for LocalLspAdapterDelegate {
         if self.fs.is_file(&working_dir).await {
             working_dir.pop();
         }
-        let output = util::command::new_smol_command(&command.path)
+        let output = util::command::new_command(&command.path)
             .args(command.arguments)
             .envs(command.env.clone().unwrap_or_default())
             .current_dir(working_dir)
